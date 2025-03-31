dad9ff6548603eb1a50c04e7e1f02f36b2490ff9
jkahiga <jazzjohn00@gmail.com>
2025-03-31 12:58:33 +0300

    DSA #1: Stack

---
+++ stack/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections;
+
+public class stack
+{
+    public static void Main()
+    {
+        // Stack creation and initialization
+        Stack myStack = new Stack();
+        myStack.Push("Hello");
+        myStack.Push("World");
+        myStack.Push("!");
+    }
+}
